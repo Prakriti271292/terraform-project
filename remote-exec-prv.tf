c281de6d2e21a6f7e96f54623494bd57539aad53
Prakriti271292 <narula2712@gmail.com>
2025-09-26 02:01:30 +0530

    Update remote-exec-prv.tf

--- remote-exec-prv.tf
+++ remote-exec-prv.tf
@@ -1,25 +1,26 @@
 resource "aws_instance" "nginx_server" {
-  ami           =  var.ami
-  instance_type =  var.instance_type
-  subnet_id = aws_subnet.public.id
-  vpc_security_group_ids = [aws_security_group.sec_grp.id]
+  ami                         = var.ami
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.public.id
+  vpc_security_group_ids      = [aws_security_group.sec_grp.id]
   associate_public_ip_address = true
 
-connection {
-  type = "ssh"
-  user = "ubuntu"
-  private_key = var.private_key
-  host = self.public_ip
-  }
-
-provisioner "remote-exec" {
-  inline = [
-    "sudo yum update -y",
-    "sudo yum install -y nginx",
-    "sudo systemctl start nginx"
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum update -y",
+      "sudo yum install -y nginx",
+      "sudo systemctl start nginx"
     ]
+
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"        
+      private_key = var.private_key
+      host        = self.public_ip
+    }
   }
 
-tags = {
-  Name = "nginx-server"
+  tags = {
+    Name = "nginx-server"
+  }
 }
