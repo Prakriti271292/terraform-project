3f3eeb1630040a731066a4886c65d6d7bedc33e0
Prakriti271292 <narula2712@gmail.com>
2025-09-26 00:38:03 +0530

    Create remote-exec-prv.tf

---
+++ remote-exec-prv.tf
@@ -0,0 +1,25 @@
+resource "aws_instance" "nginx_server" {
+  ami           =  var.ami
+  instance_type =  var.instance_type
+  subnet_id = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.sec_grp.id]
+  associate_public_ip_address = true
+
+connection {
+  type = "ssh"
+  user = "ubuntu"
+  private_key = var.private_key
+  host = self.public_ip
+  }
+
+provisioner "remote-exec" {
+  inline = [
+    "sudo yum update -y"
+    "sudo yum install -y nginx"
+    sudo systemctl start nginx"
+    ]
+  }
+
+tags = {
+  Name = "nginx-server"
+}
