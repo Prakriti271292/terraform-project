316ef2bf8fe477a39a390b35c5cc441ac1e81c60
Prakriti271292 <narula2712@gmail.com>
2025-09-25 18:02:15 +0530

    Update sec_group.tf

--- sec_group.tf
+++ sec_group.tf
@@ -8,11 +8,27 @@ resource "aws_security_group" "sec_grp" {
      }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow" {
+resource "aws_vpc_security_group_ingress_rule" "allow_tcp" {
     security_group_id = aws_security_group.sec_grp.id
     cidr_ipv4 = "0.0.0.0/0"
     from_port = var.from_port 
     to_port = var.to_port
     ip_protocol = "tcp"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+    security_group_id = aws_security_group.sec_grp.id
+    cidr_ipv4 = "0.0.0.0/0"
+    from_port = var.from_port 
+    to_port = var.to_port
+    ip_protocol = "http"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+    security_group_id = aws_security_group.sec_grp.id
+    cidr_ipv4 = "0.0.0.0/0"
+    from_port = var.from_port 
+    to_port = var.to_port
+    ip_protocol = "ssh"
 }
 
